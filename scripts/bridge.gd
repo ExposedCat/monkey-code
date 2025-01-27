701c8607a47b1d7a928206763c7e5cdb39ab28b1
Artem Prokop <artem13.prokop@gmail.com>
2025-01-27 13:31:54 +0100

    fix: ignore bridge calls when unavailable

--- scripts/bridge.gd
+++ scripts/bridge.gd
@@ -1,25 +1,27 @@
 extends Node
 
 func _init():
-  JavaScriptBridge.eval("""
-    var godotBridge = {
-      _godot_callbacks: {},
-      _godot_listen: (name, callback) => {
-        window.godotBridge._godot_callbacks[name] ??= []
-        window.godotBridge._godot_callbacks[name].push(callback)
-      },
-      _events: new EventTarget(),
-      _godot_dispatch: (name, data) => godotBridge._events.dispatchEvent(new CustomEvent(name, { detail: data })),
-      on: (name, callback) => godotBridge._events.addEventListener(name, callback),
-      off: (name, callback) => godotBridge._events.removeEventListener(name, callback),
-      dispatch: (name, args = []) => window.godotBridge._godot_callbacks[name]?.forEach(callback => callback(...args)),
-    };
+	JavaScriptBridge.eval("""
+	var godotBridge = {
+	_godot_callbacks: {},
+	_godot_listen: (name, callback) => {
+	window.godotBridge._godot_callbacks[name] ??= []
+	window.godotBridge._godot_callbacks[name].push(callback)
+	},
+	_events: new EventTarget(),
+	_godot_dispatch: (name, data) => godotBridge._events.dispatchEvent(new CustomEvent(name, { detail: data })),
+	on: (name, callback) => godotBridge._events.addEventListener(name, callback),
+	off: (name, callback) => godotBridge._events.removeEventListener(name, callback),
+	dispatch: (name, args = []) => window.godotBridge._godot_callbacks[name]?.forEach(callback => callback(...args)),
+	};
 	""", true)
 
 func register_event(event: String, callback: JavaScriptObject):
-  var bridge = JavaScriptBridge.get_interface("godotBridge")
-  bridge._godot_listen(event, callback)
+	var bridge = JavaScriptBridge.get_interface("godotBridge")
+	if bridge != null:
+		bridge._godot_listen(event, callback)
 
 func dispatch_event(event: String, data):
-  var bridge = JavaScriptBridge.get_interface("godotBridge")
-  bridge._godot_dispatch(event, JSON.stringify(data))
+	var bridge = JavaScriptBridge.get_interface("godotBridge")
+	if bridge != null:
+		bridge._godot_dispatch(event, JSON.stringify(data))
