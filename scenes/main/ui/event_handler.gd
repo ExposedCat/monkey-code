895c2a4e9770e5afa0d030b907fe7b141a6ecce9
Artem Prokop <artem13.prokop@gmail.com>
2025-02-04 03:49:05 +0100

    feat: pause, button styles

--- scenes/main/ui/event_handler.gd
+++ scenes/main/ui/event_handler.gd
@@ -1,4 +1,20 @@
 extends CanvasLayer
 
+var editor_opened = false
+
+func toggle_pause():
+	var tree = get_tree()
+	tree.paused = not tree.paused
+	return tree.paused
+
 func _on_open_editor_pressed() -> void:
-	JavaScriptBridge.eval("window.dispatchEvent(new CustomEvent('open-editor'))")
+	toggle_pause()
+	var dispatcher = "window.dispatchEvent(new CustomEvent('%s-editor'))" % ["open" if not editor_opened else "close"]
+	$"Open Editor".text = "Open Editor" if editor_opened else "Close Editor"
+	editor_opened = not editor_opened
+	JavaScriptBridge.eval(dispatcher)
+
+func _on_pause_pressed() -> void:
+	var paused = toggle_pause()
+	$ColorRect.visible = paused
+	$Pause.text = "Continue" if paused else "Pause"
