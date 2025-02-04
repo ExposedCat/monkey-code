f5634b6fd58a784ab48ebaea2afec662b07aea8b
Artem Prokop <artem13.prokop@gmail.com>
2025-02-04 02:46:35 +0100

    feat: styled editor button

--- client/build.sh
+++ client/build.sh
@@ -4,8 +4,9 @@ rm -rf ../dist/client
 
 npx rollup -c
 
-mkdir -p ../dist/client
+mkdir -p ../dist/client/assets
 
 cp -r ./dist/bundle.js ./src/style ../dist/client
+cp -r ../assets/ui/buttons ../dist/client/assets
 
 echo "var gameSdkTypes = \`$(cat ./src/typedefs/sdk.d.ts)\`" > ../dist/client/sdk-types.js
