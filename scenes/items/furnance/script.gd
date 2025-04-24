5f306fc7e117b125a4b3677edc2cb3c9050b796d
Artem Prokop <artem13.prokop@gmail.com>
2025-04-24 20:18:12 +0200

    feat: migrate to 4.4

--- scenes/items/furnance/script.gd
+++ scenes/items/furnance/script.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if sprite.animation == "cooking":
 		return
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed == false:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and not event.pressed:
 		var loaded = InventoryManager.player.remove_item("stone", 1)
 		if not loaded:
 			return
